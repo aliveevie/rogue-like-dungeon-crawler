26b8c85310308ed054540621655e554910eb0d1e
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-08-10 11:03:08 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+const numRows = 10;
+const numCols = 10;
 
 function App() {
+
+  const [playerPos, setPlayerPos] = useState({ x: 0, y: 0 });
+  const [enemies, setEnemies] = useState([{ x: 3, y: 4 }]);
+
+  const handleKeyPress = (e) => {
+    // Update player position based on key presses
+    // You would need to implement collision detection with walls and enemies here
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+    return () => window.removeEventListener('keydown', handleKeyPress);
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="game-area">
+        {Array.from({ length: numRows }).map((_, row) =>
+          Array.from({ length: numCols }).map((_, col) => (
+            <div
+              key={`${row}-${col}`}
+              className={`cell ${playerPos.x === col && playerPos.y === row ? 'player' : ''}`}
+            >
+              {enemies.some(enemy => enemy.x === col && enemy.y === row) && (
+                <div className="enemy"></div>
+              )}
+            </div>
+          ))
+        )}
+      </div>
       </header>
     </div>
   );
