436a2fa5fd185a93d649f84f359d60ed65d9e601
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-08-12 11:14:13 +0100

    modified files

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+
 
 export function Game(){
 const [xp, setXp] = useState(30);
@@ -8,15 +9,34 @@ const [weapon, setWeapon] = useState('Knife');
 const [damage, setDamage] = useState(15);
 const [enemies, setEnemies] = useState(10);
 
+const [map, setMap] = useState(Array(100).fill(null)
+    .map(() => Array(100)
+    .fill({ x: 0, y:0 })));
+
+const myCanvas = useRef(null);
+const ctxRef = useRef(null);
+
+
+useEffect(() => {
+    const canvas = myCanvas.current;
+    const context = canvas.getContext('2d')
+    ctxRef.current = context;
+    draw();
+}, []);
+
+
+function draw(){
+    const ctx = ctxRef.current
+
+    ctx.beginPath();
+    ctx.rect(0,0,10,10);
+    ctx.fillStyle = 'red';
+    ctx.fill();
+    ctx.closePath();
+}
 
     return (
-        <div >
-              <canvas
-              id="mycanvas"
-              width={600}
-              height={400}
-              >
-            </canvas> 
+        <div > 
           <div className="game-area" >
           <p>xP: {xp} - </p>
                 <p>Level: {level} - </p>
@@ -25,7 +45,11 @@ const [enemies, setEnemies] = useState(10);
                 <p>Damage:  {damage} - </p>
                 <p>Enemies Left:  {enemies}</p>
           </div>
-          
+          <canvas
+          ref={myCanvas}
+          width={600}
+          height={400}
+          />
         </div>
         
     )
